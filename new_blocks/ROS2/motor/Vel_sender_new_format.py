d7e67f089d3bd623470b4b1e4af8546097631459
David Tapiador <d.tapiador.2018@alumnos.urjc.es>
2022-07-15 12:56:01 +0200

    Updating the motor driver block to new version of blocks

---
+++ new_blocks/ROS2/motor/Vel_sender_new_format.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def main(inputs, outputs, parameters, synchronise):
+
+    auto_enable = False
+    try:
+        enable = inputs.read_number('Enable')
+    except Exception:
+        auto_enable = True
+
+    try:
+        while auto_enable or inputs.read_number('Enable'):
+            vels = [1,0,0,0,0,0]
+            to_write = np.array(vels, dtype='<U64')
+            outputs.share_string('Vels', vels)   
+            synchronise()
+    
+    except Exception as e:
+        print("Error")
